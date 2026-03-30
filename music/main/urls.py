569e35b16cb3c6050af153b68f9a3fba4c28b608
Kirzhan <kirill@gmail.com>
2026-03-30 14:36:04 +0700

    мой первый комит

---
+++ music/main/urls.py
@@ -0,0 +1,12 @@
+﻿from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('genres/', views.genres, name='genres'),
+    path('tracks/', views.tracks, name='tracks'),
+    path('add_genre/', views.add_genre),
+    path('editgenre/<int:id_genre>', views.add_genre),
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
---
+++ music/main/views.py
@@ -0,0 +1,47 @@
+﻿from django.shortcuts import redirect, render
+from .models import Genre, Track
+from .forms import GenreForm
+
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def genres(request):
+    genres_list = Genre.objects.all()
+    return render(request, "main/genres.html", {"genres": genres_list})
+
+
+def tracks(request):
+    tracks_list = Track.objects.prefetch_related("genres").all()
+    return render(request, "main/tracks.html", {"tracks": tracks_list})
+
+def add_genre(request):
+    if request.method == "POST":
+        genre = GenreForm(request.POST)
+        if genre.is_valid():
+            genre.save()
+        return redirect('/genres')
+    else:
+        genreform = GenreForm()
+        return render(request, "add_genre.html", {'form': genreform})
+    
+def edit_genre(request, id_genre):
+    g = Genre.objects.get(id=id_genre)
+
+    if request.method == "POST":
+
+        genre = GenreForm(request.POST, instance=g)
+
+        if genre.is_valid():
+            genre.save()
+
+        return redirect('/genres')
+
+    else:
+        genreform = GenreForm(instance=g)
+        return render(request, "add_genre.html", {'form': genreform})
+    
+
+
